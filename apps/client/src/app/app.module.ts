ea9c1130289b83a00669bb6a09bf77f5d437b2c3
Maxim Tarasov <31613333+TarasovMV@users.noreply.github.com>
2022-11-18 01:05:10 +0300

    fix(client): Загрузка PWA

--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -1,15 +1,22 @@
-import {TuiRootModule, TuiDialogModule, TuiAlertModule, TuiModeModule, TuiThemeNightModule} from "@taiga-ui/core";
-import {TUI_LANGUAGE, TUI_RUSSIAN_LANGUAGE} from '@taiga-ui/i18n';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {NgModule, isDevMode} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import {
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule,
+    TuiModeModule,
+    TuiThemeNightModule,
+} from '@taiga-ui/core';
+import { TUI_LANGUAGE, TUI_RUSSIAN_LANGUAGE } from '@taiga-ui/i18n';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule, isDevMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {AppRoutingModule} from './app-routing.module';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {AuthInterceptor} from './interceptors/auth.interceptor';
-import {of} from 'rxjs';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { of } from 'rxjs';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
     declarations: [AppComponent],
@@ -25,10 +32,10 @@ import { ServiceWorkerModule } from '@angular/service-worker';
         TuiModeModule,
         TuiThemeNightModule,
         ServiceWorkerModule.register('ngsw-worker.js', {
-          enabled: !isDevMode(),
-          // Register the ServiceWorker as soon as the application is stable
-          // or after 30 seconds (whichever comes first).
-          registrationStrategy: 'registerWhenStable:30000'
+            enabled: !(() => isDevMode())(),
+            // Register the ServiceWorker as soon as the application is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000',
         }),
     ],
     providers: [
@@ -44,5 +51,4 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     ],
     bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
