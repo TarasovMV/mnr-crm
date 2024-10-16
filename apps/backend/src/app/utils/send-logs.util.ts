d496b8d425506caf1d41610d5c66907955310073
Maxim Tarasov <31613333+TarasovMV@users.noreply.github.com>
2024-10-16 16:00:53 +0300

    fix: tmp logs disable

--- apps/backend/src/app/utils/send-logs.util.ts
+++ apps/backend/src/app/utils/send-logs.util.ts
@@ -3,5 +3,6 @@ import {HttpService} from '@nestjs/axios';
 const LOG_URL = 'https://crm-connector.maxim-trsv.ru';
 
 export function sendLogsUtil<T>(http: HttpService, type: string, data: T) {
-    return http.post(`${LOG_URL}/api/data?crm=mnr&type=${type}`, data).subscribe();
+    return;
+    // return http.post(`${LOG_URL}/api/data?crm=mnr&type=${type}`, data).subscribe();
 }
